4f66f74a93691541664eb79c966b252eb258bf33
Ali-Ayb <aliayoub.me1@gmail.com>
2023-02-24 22:04:56 +0200

    added a title change functionality accourding to the random numbers generated

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let player2_num = Math.ceil(Math.random() * 6);
 
 const dice1 = document.getElementById("dice1");
 const dice2 = document.getElementById("dice2");
-
+const title = document.getElementById("title");
 const img = document.createElement("img");
 img.src = "dot_img.png";
 
@@ -11,3 +11,11 @@ const dotHTML = '<img class="dot-image" src="dot_img.png">';
 
 dice1.innerHTML = dotHTML.repeat(player1_num);
 dice2.innerHTML = dotHTML.repeat(player2_num);
+
+if (player1_num > player2_num) {
+  title.innerText = "Player 1 win !!!";
+} else if (player2_num > player1_num) {
+  title.innerText = "Player 2 win !!!";
+} else {
+  title.innerText = "Draw :(";
+}
