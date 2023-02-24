1a38becd6d8d3d39e6223f2979049b05090fbaf7
Ali-Ayb <aliayoub.me1@gmail.com>
2023-02-24 22:16:45 +0200

    changed the dotHTML to dot_HTML

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let player1_num = Math.ceil(Math.random() * 6);
-let player2_num = Math.ceil(Math.random() * 6);
+const player1_num = Math.ceil(Math.random() * 6);
+const player2_num = Math.ceil(Math.random() * 6);
 
 const dice1 = document.getElementById("dice1");
 const dice2 = document.getElementById("dice2");
@@ -7,10 +7,10 @@ const title = document.getElementById("title");
 const img = document.createElement("img");
 img.src = "dot_img.png";
 
-const dotHTML = '<img class="dot-image" src="dot_img.png">';
+const dot_HTML = '<img class="dot-image" src="dot_img.png">';
 
-dice1.innerHTML = dotHTML.repeat(player1_num);
-dice2.innerHTML = dotHTML.repeat(player2_num);
+dice1.innerHTML = dot_HTML.repeat(player1_num);
+dice2.innerHTML = dot_HTML.repeat(player2_num);
 
 if (player1_num > player2_num) {
   title.innerText = "Player 1 win !!!";
